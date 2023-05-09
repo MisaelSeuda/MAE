e674a0b6f65286ae7c223dc069b75e6b4ae8d61a
Misa Misa <44177887+MisaelSeuda@users.noreply.github.com>
2023-05-08 21:42:40 -0300

    Create a component with help copilot ia

--- Client-FrontEnd-React/src/App.js
+++ Client-FrontEnd-React/src/App.js
@@ -4,45 +4,9 @@ import CadastroMentorias from "./screens/CadastroMentorias";
 import RelatorioAtividades from "./screens/RelatorioAtividades";
 import CadastroProjetos from "./screens/CadastroProjetos";
 import CadastroMentor from "./screens/CadastroMentor";
-import CadastroAcompanhamento from "./screens/CadastroAcompanhamento"
-import Login from "./screens/Login"
-import "../src/styles/App.css";
-
-function Sidebar() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleSidebar = () => {
-    setIsOpen(!isOpen);
-  };
-
-  return (
-    <>
-      <button className="sidebar-toggle" onClick={toggleSidebar}>
-        <span>&#9776;</span>
-      </button>
-
-      <nav style={{ transform: `translateX(${isOpen ? 0 : "-100%"})` }}>
-        <ul>
-          <li>
-            <NavLink to="/cadastro-mentorias" activeClassName="active">Cadastro de Mentorias</NavLink>
-          </li>
-          <li>
-            <NavLink to="/relatorio-atividades" activeClassName="active">Relatório de Atividades</NavLink>
-          </li>
-          <li>
-            <NavLink to="/cadastro-projetos" activeClassName="active">Cadastro de Projetos</NavLink>
-          </li>
-          <li>
-            <NavLink to="/cadastro-mentor" activeClassName="active">Cadastro de Mentor</NavLink>
-          </li>
-          <li>
-            <NavLink to="/cadastro-acompanhamento" activeClassName="active">Cadastro de Acompanhamento</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </>
-  );
-}
+import CadastroAcompanhamento from "./screens/CadastroAcompanhamento";
+import Login from "./screens/Login";
+import Sidebar from "../src/components/Sidebar";
 
 function App() {
   return (
