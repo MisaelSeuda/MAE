11c7ff1351ff696163fa5e48bd62c4c637857b9f
Misa Misa <44177887+MisaelSeuda@users.noreply.github.com>
2023-05-12 16:59:44 -0300

    Update sidebar

--- Client-FrontEnd-React/src/App.js
+++ Client-FrontEnd-React/src/App.js
@@ -1,40 +1,47 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ProSidebarProvider, Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 import CadastroMentorias from "./screens/CadastroMentorias";
 import RelatorioAtividades from "./screens/RelatorioAtividades";
 import CadastroProjetos from "./screens/CadastroProjetos";
 import CadastroMentor from "./screens/CadastroMentor";
-import CadastroAcompanhamento from "./screens/CadastroAcompanhamento";
 import Login from "./screens/Login";
-import Sidebar from "../src/components/Sidebar";
+import { Link } from 'react-router-dom';
+import CadastroAcompanhamento from "./screens/CadastroAcompanhamento";
+
+function AppWithProSidebar() {
+  return (
+    <ProSidebarProvider>
+      <div style={{ display: 'flex', height: '100%', minHeight: '100px'}}>
+        <Sidebar>
+          <Menu >
+            <MenuItem component={<Link to="/cadastro-mentorias" />}>Cadastro Mentorias</MenuItem>
+            <MenuItem component={<Link to="/relatorio-atividades" />}>Relatorio Atividades</MenuItem>
+            <MenuItem component={<Link to="/cadastro-projetos" />}>Cadastro Projetos</MenuItem>
+            <MenuItem component={<Link to="/cadastro-mentor" />}>Cadastro Mentor</MenuItem>
+            <MenuItem component={<Link to="/cadastro-acompanhamento" />}>Cadastro Acompanhamento</MenuItem>
+          </Menu>
+        </Sidebar>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/cadastro-mentorias" element={<CadastroMentorias />} />
+          <Route path="/relatorio-atividades" element={<RelatorioAtividades />} />
+          <Route path="/cadastro-projetos" element={<CadastroProjetos />} />
+          <Route path="/cadastro-mentor" element={<CadastroMentor />} />
+          <Route path="/cadastro-acompanhamento" element={<CadastroAcompanhamento />} />
+        </Routes>
+      </div>
+    </ProSidebarProvider>
+  );
+}
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/*" element={<PrivateRoutes />} />
+        <Route path="/*" element={<AppWithProSidebar />} />
       </Routes>
     </Router>
-  );
-}
-
-function PrivateRoutes() {
-  // verificando se a página atual é a de login
-  const isLoginPage = window.location.pathname === "/";
-
-  return (
-    <>
-      {!isLoginPage && <Sidebar />} {/*renderizando a Sidebar somente se não estiver na página de login*/}
-      <Routes>
-        <Route path='/cadastro-mentorias' element={<CadastroMentorias />} />
-        <Route path="/relatorio-atividades" element={<RelatorioAtividades />} />
-        <Route path="/cadastro-projetos" element={<CadastroProjetos />} />
-        <Route path="/cadastro-mentor" element={<CadastroMentor />} />
-        <Route path="/cadastro-acompanhamento" element={<CadastroAcompanhamento />} />
-        {/* adicione outras rotas privadas aqui */}
-      </Routes>
-    </>
   );
 }
 
